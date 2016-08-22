98ae91d6d544ef360ed75c77f3f2de7de8c6ce7b
Grace_ji <80709412@qq.com>
2016-08-22 11:25:22 +0800

    .babelrc

--- src/funProgram/funProgram.entry.js
+++ src/funProgram/funProgram.entry.js
@@ -1,7 +1,3 @@
 // 导入 进入点的宿主 html 和 style 依赖
-import './style.scss';
-import 'mobile-responsive';
-
-// $(document).ready(() => {
-//   // $('#carousel-example-generic').carousel()
-// })
+import "./style.scss";
+import "mobile-responsive";
--- src/mobileBoilerplate/mobileBoilerplate.entry.js
+++ src/mobileBoilerplate/mobileBoilerplate.entry.js
@@ -1,6 +1,6 @@
-// 导入 进入点的宿主 html 和 style 依赖
-import './style.scss';
-import 'mobile-responsive';
+// // 导入 进入点的宿主 html 和 style 依赖
+// import './style.scss';
+// import 'mobile-responsive';
 
 // $(document).ready(() => {
 //   // $('#carousel-example-generic').carousel()
